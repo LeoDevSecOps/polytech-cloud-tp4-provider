fd9ba6195ae1fce6781e615c2b2897fb430d0dd0
LeoDevSecOps <leoheritier@gmail.com>
2021-02-22 18:49:36 +0100

    feat(name): added name to provider

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  name: process.env.NAME || "Leo",
   port: process.env.PORT || 8081,
   host: process.env.HOST || 'http://localhost',
   register_url: process.env.API_REGISTER || 'https://app-63253679-f9ca-444c-ac1a-f7d17fcfed2c.cleverapps.io/providers'
