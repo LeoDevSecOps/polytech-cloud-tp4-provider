a4bee805231e9430082aaacdcd1ec4c4cab5acfd
LeoDevSecOps <leoheritier@gmail.com>
2021-02-22 09:38:12 +0100

    fix(host)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 8081,
   host: process.env.HOST || 'http://localhost',
-  register_url: process.env.API_REGISTER || 'http://localhost:8082/providers'
+  register_url: process.env.API_REGISTER || 'https://app-63253679-f9ca-444c-ac1a-f7d17fcfed2c.cleverapps.io/providers'
 }
   
